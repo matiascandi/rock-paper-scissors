73666e6e4d3e48e2a2e89514c9413b6bbfb11b62
Matias Candi <63614139+matiascandi@users.noreply.github.com>
2024-01-19 19:31:50 +0100

    finished with all the functions. Missing CSS

--- javascript.js
+++ javascript.js
@@ -2,34 +2,46 @@ function getComputerChoice(choice) {
     return choice[Math.floor(Math.random() * choice.length)]
 }
 let rps = ["Rock", "Paper", "Scissors"];
-let playerSelection = prompt("Rock, paper or scissors?")
-let computerSelection = getComputerChoice(rps);
-let playerCounter = 0;
-let computerCounter = 0;
-
-//function addPlayerCounter(){
- //   computerCounter + 1;
-//}
-//function addComputerCounter(){
-//    computerCounter + 1;
-//}
+let playerScore = 0;
+let computerScore = 0;
+let roundsToPlay = 5;
 
 function playRound(playerSelection, computerSelection) {
-    if (computerSelection.toLowerCase() == "Scissors" && playerSelection.toLowerCase() == "Paper",
-        computerSelection.toLowerCase() == "Rock" && playerSelection.toLowerCase() == "Scissors",
-        computerSelection.toLowerCase() == "Paper" && playerSelection.toLowerCase() == "Rock") {
-            //addComputerCounter();
-            return "Computer wins! Try again";
+    let playerChoice = playerSelection.trim().toLowerCase();
+    let computerChoice = computerSelection.toLowerCase();
+    
+    if (computerChoice == "scissors" && playerChoice == "paper" ||
+        computerChoice == "rock" && playerChoice == "scissors" ||
+        computerChoice == "paper" && playerChoice == "rock"){
+        computerScore++;
+        return "Computer wins! Try again";
     }
-    else if (computerSelection.toLowerCase() === playerSelection) {
+    else if (computerChoice == playerChoice) {
         return "That's a tie! Play again!";
     }
-    else {
-        //addPlayerCounter;
-        return "You won! Congratulations";
+    else if (playerChoice == "scissors" && computerChoice == "paper" ||
+    playerChoice == "rock" && computerChoice == "scissors" ||
+    playerChoice == "paper" && computerChoice == "rock"){
+        playerScore++;
+        return "You won! Congratulations!";
     }
+    return "Invalid selections. Please choose Rock, Paper, or Scissors";
 }
 
+function game(){
+    for (let round = 1; round <= roundsToPlay; round++) {
+        let playerSelection = prompt("Rock, paper or scissors?");
+        let computerSelection = getComputerChoice(rps);
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(playerScore);
+        console.log(computerScore);
+    }
+    if (playerScore === 3){
+        console.log("Congratulations! You won the game!");
+    } else if (computerScore === 3){
+        console.log("Sorry, you lost. Try again!");
+    }
+    return "invalid";
+}
 
-console.log(playRound(playerSelection, computerSelection));
-console.log(playerCounter);
+game();
